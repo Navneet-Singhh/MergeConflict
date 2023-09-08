f88bacd0fb9c9cabcd8c7404026a33329ac268d7
Navneet-Singhh <navneetxperi@gmail.com>
2023-09-08 20:17:25 +0530

    ejnfl

--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -2,9 +2,7 @@ package org.example;
 
 public class Test {
     public static void main(String[] args) {
-        String hsn="";
-        try {
-            if(hsn.isEmpty()){
+        String hsn="";thsn.isEmpty()){
                 System.out.println("isEmpty is handling null as well");}
         }catch (Exception e){
             e.printStackTrace();
